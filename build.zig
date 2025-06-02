2ff09a0dce1e9041f5731b4e463f7eb0e972c0e6
Gabriel Colbath <gabe@Gabriels-MacBook-Pro.local>
2025-06-02 14:39:53 -0400

    Rewrite on server and app functionality.

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+
     const optimize = b.standardOptimizeOption(.{});
 
     const exe_mod = b.createModule(.{
@@ -11,22 +12,22 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "scatty",
+        .name = "reasonable_actions_dev",
         .root_module = exe_mod,
-    });
-
-    const httpz = b.dependency("httpz", .{
-        .target = target,
-        .optimize = optimize,
     });
 
     const uuid = b.dependency("uuid", .{
         .target = target,
         .optimize = optimize,
     });
+    
+    const httpz = b.dependency("httpz", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    exe.root_module.addImport("httpz", httpz.module("httpz")); 
     exe.root_module.addImport("uuid", uuid.module("uuid"));
+    exe.root_module.addImport("httpz", httpz.module("httpz"));
 
     b.installArtifact(exe);
 
@@ -41,28 +42,12 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const test_step = b.step("test", "Run unit tests."); 
-
-    const app_test = b.addTest(.{
-        .root_source_file = b.path("src/app.zig"),
-        .target = target,
-        .optimize = optimize,
+    const exe_unit_tests = b.addTest(.{
+        .root_module = exe_mod,
     });
 
-    const server_test = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    app_test.root_module.addImport("httpz", httpz.module("httpz"));
-    app_test.root_module.addImport("uuid", uuid.module("uuid"));
-
-    server_test.root_module.addImport("httpz", httpz.module("httpz"));
-    server_test.root_module.addImport("uuid", uuid.module("uuid"));
-
-    const run_app_tests = b.addRunArtifact(app_test);
-    const run_server_tests = b.addRunArtifact(server_test);
-    test_step.dependOn(&run_app_tests.step);
-    test_step.dependOn(&run_server_tests.step);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/config.zig
+++ src/config.zig
@@ -1,21 +1,5 @@
 const std = @import("std");
 
-// Server
-pub const port = 8801;
-pub const max_room_list_items = 50;
-
-// App
-pub const max_total_members = 100;
-pub const max_members_per_room = 8;
-pub const max_total_rooms = 100;
-
-// Game
-pub const base_categories_list = @embedFile(categories_list_filepath ++ "base.txt");
-pub const custom_categories_filepath = @as(usize, categories_list_filepath ++ "custom.txt");
-pub const categories_list_filepath = @as([]const u8, "categories/");
-pub const max_players_per_game = max_members_per_room;
-pub const max_category_len = 128;
-pub const max_num_categories = 256;
-pub const max_categories_list_size = max_category_len * max_num_categories;
-pub const max_answer_len = 128;
-pub const valid_letters: []const u8 = "ABCDEFGHIJKLMNOPRSTW";
+pub const num_rooms_capacity = 100;
+pub const num_members_capacity = 1000;
+pub const num_members_per_room_capacity = 8;
